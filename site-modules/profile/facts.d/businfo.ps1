da131e8a36ee927afdd6927d8d45aecd14b61db5
Leo Abreu <leo.abreu@puppet.com>
2021-04-25 15:47:54 -0400

    base ps1 code to set business unit on windows

--- site-modules/profile/facts.d/businfo.ps1
+++ site-modules/profile/facts.d/businfo.ps1
@@ -1,1 +1,10 @@
 $hostname = get-content env:computername
+Write-Host -foregroundcolor Yellow "Computername is $hostname"
+if ($hostname.StartsWith('r')){
+    Write-Host "business_unit=retail"
+}elseif ($hostname.StartsWith('b')){
+    Write-Host "business_unit=banking"
+}else {
+    Write-Host "business_unit=it"
+}
+
